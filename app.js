67cadfe5e4ad86eabe1a050377c44f7fe0f5a78c
Uni <janssen.malte@gmx.net>
2018-07-05 01:47:36 +0200

    + Server API, graphing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const chalk = require('chalk');
+const axios = require('axios');
 
 function log(component, msg) {
 	console.log(`${chalk.green(`:: ${component} ::`)} ${msg}`);
@@ -9,9 +10,26 @@ function error(component, msg) {
 	console.log(`${chalk.red(`:: ${component} ::`)} ${msg}`);
 }
 
+function today() {
+	const d = new Date;
+	return String(d.getFullYear()) + ((d.getMonth()+1) > 9 ? (d.getMonth()+1) : String(0) + (d.getMonth()+1)) + (d.getDate() > 9 ? d.getDate() : String(0) + d.getDate());
+}
+
 const app = express();
 
 app.use(express.static('static'));
+
+app.get('/api/:article', (req, res) => {
+	log('Express', `${req.params.article} 🕔`);
+	const url = `https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/${req.params.article}/daily/20180101/${today()}`;
+	axios.get(url).then(response => {
+		log('Express', `${req.params.article} ✔️`);
+		res.send(response.data.items);
+	}).catch(err => {
+		error('Express', `${req.params.article} ❌`);
+		res.status(500).send();
+	})
+});
 
 app.listen(8080, () => {
 	log('Express', 'Listening on port 8080');
--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,41 @@ const wait = t => new Promise(r => setTimeout(r, t));
 
 qE('.search-btn', 'click', e => {
 	qS('.graph-cont').classList.add('hidden');
-	wait(500).then(() => {
+
+	fetch(`/api/${qS('.search-input').value.replace(/\s/g, '_')}`).then(r => r.json()).then(r => {
+		const data = r.map(x => x.views);
+		const labels = r.map(x => x.timestamp);
+
+		const ctx = qS('.graph').getContext('2d');
+
+		const gradient = ctx.createLinearGradient(0, 0, 640, 360);
+		gradient.addColorStop(0, "#036ED9");
+		gradient.addColorStop(1, "#0FF0B3");
+
+		const chart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels,
+				datasets: [{
+					data,
+					backgroundColor: gradient,
+					pointRadius: 0,
+					pointHitRadius: 3,
+					borderColor: '#f0f0f0'
+				}]
+			},
+			options: {
+				legend: false,
+				scales: {
+					yAxes: [{
+						display: false
+					}],
+					xAxes: [{
+						display: false
+					}]
+				}
+			}
+		});
 		qS('.graph-cont').classList.remove('hidden');
-	})
+	});
 });
